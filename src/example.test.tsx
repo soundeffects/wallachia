3ae5582f8c6f58d6f23eca83ee48e841038cf90e
soundeffects <james@youngbloods.org>
2021-08-16 12:42:34 -0600

    Remove unused src code

---
+++ src/example.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+
+describe('<App>', () => {
+  it('renders learn react link', () => {
+    //const { getByText } = render();
+    //const linkElement = getByText(/learn react/i);
+    //expect(document.body.contains(linkElement));
+  });
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*App code goes here*/}
   </React.StrictMode>,
   document.getElementById('root'),
 );
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
